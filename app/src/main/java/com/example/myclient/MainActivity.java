796f61282e4f7580f039f186fc51407da2ba62ad
Aleksey Gilenko <aleksey@gilenko.net>
2026-02-21 22:56:36 +0300

    Добавлен mbedtls

--- app/src/main/java/com/example/myclient/MainActivity.java
+++ app/src/main/java/com/example/myclient/MainActivity.java
@@ -7,11 +7,15 @@ import android.widget.TextView;
 
 import com.example.myclient.databinding.ActivityMainBinding;
 
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
 
     // Used to load the 'myclient' library on application startup.
     static {
         System.loadLibrary("myclient");
+        System.loadLibrary("mbedcrypto");
+        LogUsingJNI("All libraries successfully loaded");
     }
 
     private ActivityMainBinding binding;
@@ -23,6 +27,17 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        int res = initRng();
+        byte[] key = randomBytes(16);
+        byte[] data = {10, 12, 32, 14};
+        LogUsingJNI("Data: " + Arrays.toString(data));
+        LogUsingJNI("Generated key: " + Arrays.toString(key));
+
+        byte[] encrypted = encrypt(key, data);
+        LogUsingJNI("Data encrypted with 3DES: " + Arrays.toString(encrypted));
+        byte[] decrypted = decrypt(key, encrypted);
+        LogUsingJNI("Data decrypted: " + Arrays.toString(decrypted));
+
         // Example of a call to a native method
         TextView tv = binding.sampleText;
         tv.setText(stringFromJNI());
@@ -33,4 +48,11 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+    public static native void LogUsingJNI(String str);
+
+    public static native int initRng();
+    public static native byte[] randomBytes(int no);
+
+    public static native byte[] encrypt(byte[] key, byte[] data);
+    public static native byte[] decrypt(byte[] key, byte[] data);
 }
