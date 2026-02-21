3a166ad25c99865e575555e15dae751089387807
Aleksey Gilenko <aleksey@gilenko.net>
2026-02-21 14:31:14 +0300

    Initial Commit

---
+++ app/src/main/java/com/example/myclient/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.myclient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.myclient.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'myclient' library on application startup.
+    static {
+        System.loadLibrary("myclient");
+    }
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Example of a call to a native method
+        TextView tv = binding.sampleText;
+        tv.setText(stringFromJNI());
+    }
+
+    /**
+     * A native method that is implemented by the 'myclient' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
